1bc9cd11085524ad87524c3e71a2d3fb4dc4903e
Pratik Padnure <154249284+ppadnure28@users.noreply.github.com>
2026-08-04 16:32:56 +0530

    Delete Tiffino-User_Login directory

--- Tiffino-User_Login/src/main/java/com/tiffino/user/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiffino.user.repository;
-
-import com.tiffino.user.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-    List<Address> findByUserId(Long userId);
-}
